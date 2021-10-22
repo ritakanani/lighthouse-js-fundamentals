27dcbd55854c55788ea7ce551c174a8d60b6f59a
Ritaben Vaghasia <ritakanani@yahoo.com>
2021-10-22 17:25:12 -0400

    declaring and calling function

--- hello.js
+++ hello.js
@@ -4,10 +4,29 @@ const sayHello = function() {
   console.log("Hello world");
 }
 
-sayHello();
+sayHello(); // print Hello world
 
 // 2))
 
 const sayHello = function(name) {
   console.log("Hello, " + name);
+};                // no declare the value of name
+
+// 3)) // console.log:
+
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
 }
+sayHelloToConsole('John');  // print Hello, John
+
+// 4)) // return:
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello("John"); // print undefined
+
+// 5)
+
+console.log(greeting); // print Hello, John

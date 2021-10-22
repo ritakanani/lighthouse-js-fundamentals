428600eebc680c00d29d3e883db3bf538ffeb700
Ritaben Vaghasia <ritakanani@yahoo.com>
2021-10-22 16:59:47 -0400

    declaring and calling function

---
+++ even.js
@@ -0,0 +1,12 @@
+// 1)
+
+const isEven = function(num) {
+  return num % 2 === 0;
+};
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);  // Print true
+console.log(elevenIsEven);  // Print false
+
